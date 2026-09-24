325423d3995f0f12d2621dc5b4140edbf29123b3
floricemumu557-glitch <floricejm557@gmail.com>
2026-09-24 18:33:40 +0000

    Initialisation du projet Spring Boot

---
+++ src/main/java/com/example/gestionmateriels/GestionMaterielsApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestionmateriels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionMaterielsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionMaterielsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gestionmateriels/GestionMaterielsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestionmateriels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionMaterielsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
